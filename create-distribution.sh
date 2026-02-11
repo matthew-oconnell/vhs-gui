5a407a6e40823b172c76f924cbcd9c730451d1c5
Matthew O'Connell <Matthew.D.OConnell@nasa.gov>
2026-02-11 08:15:12 -0500

    change version number to 0.2.0

--- create-distribution.sh
+++ create-distribution.sh
@@ -5,7 +5,7 @@
 set -e
 
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-VERSION="1.0.0"
+VERSION="0.2.0"
 DIST_NAME="vhs-gui-v${VERSION}"
 
 echo "======================================"
